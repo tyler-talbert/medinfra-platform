510c35aefa7848931a1c5e5eb63ff065f8ea2e96
Tyler Talbert <tylertalbert02@gmail.com>
2025-05-26 14:29:37 -0400

    Configuration changes

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,23 +1,26 @@
 package com.pm.billingservice.grpc;
 
+import billing.BillingRequest;
 import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
-import billing.BillingServiceGrpc.BillingServiceImplBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @GrpcService
-public class BillingGrpcService extends BillingServiceImplBase{
-    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(
+            BillingGrpcService.class);
 
     @Override
-    public void createBillingAccount(billing.BillingRequest billingRequest,
+    public void createBillingAccount(BillingRequest billingRequest,
                                      StreamObserver<BillingResponse> responseObserver) {
 
-        log.info("createBillingAccount request recieved {}", billingRequest.toString());
+        log.info("createBillingAccount request received {}", billingRequest.toString());
 
-        // TODO: Business logic - e.g save to DB, perform calculations etc
+        // Business logic - e.g save to database, perform calculates etc
 
         BillingResponse response = BillingResponse.newBuilder()
                 .setAccountId("12345")
